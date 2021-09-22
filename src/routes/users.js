acc1243510aeae53fc23abcb75b98191e2ed2b79
AliHaidry <alihaidry11@gmail.com>
2021-09-22 16:13:38 -0400

    Code Updated

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const UserRepo = require('../repos/user-repo');
 
 
 const router = express.Router();
 
 router.get('/users', async (req, res) => {
+    // Run a query to get all users
+    const users = await UserRepo.find();
 
+    // Send the result back to the person
+    // whom made this request
+    res.send(users);    
 });
 
 router.get('/users/:id', async (req, res) => {
+    const {id} = req.params;
+
+    const user = await UserRepo.findById(id);
+
+    if (user) {
+        res.send(user);
+    }
+    else {
+        res.sendStatus(404);
+    }
 
 });
 
