ff177566a9a9b34cf9e1a0372a645208164fc2d8
AliHaidry <alihaidry11@gmail.com>
2021-09-23 11:24:25 -0400

    Code Updated

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -13,9 +13,9 @@ class UserRepo {
     }
 
     static async findById(id){
-        const {rows} = await pool.query(
-            `SELECT * FROM users WHERE id = ${id};
-        `);
+        const {rows} = await pool.query(`
+            SELECT * FROM users WHERE id = $1;
+        `,[id]);
 
         return toCamelCase(rows)[0];
 
